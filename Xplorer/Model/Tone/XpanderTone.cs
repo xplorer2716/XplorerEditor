0c390df87c930949641a8b6cf3f4fb6858549837
xplorer <xplorer@github.com>
2026-02-18 23:24:28 +0100

    update dates. + publish single file

--- Xplorer/Model/Tone/XpanderTone.cs
+++ Xplorer/Model/Tone/XpanderTone.cs
@@ -1,6 +1,6 @@
 /*
 Xplorer - A real-time editor for the Oberheim Xpander and Matrix-12 synths
-Copyright (C) 2012-2024 Pascal Schmitt
+Copyright (C) 2012-2026 Pascal Schmitt
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
